6483adcec6358ee9786d682a6a4efcd4b475b1a7
zavet-code <yauheni.zavatski13@gmail.com>
2021-08-02 18:20:04 +0300

    add def with list sqrt numbers +-

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print(Hello)
+import math
+
+def get_square_roots(num):
+    defult_list = []
+    defult_list.append(-math.sqrt(num))
+    defult_list.append(math.sqrt(num))
+    return print(defult_list)
+
+get_square_roots(25)
+
