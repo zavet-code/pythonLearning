ebeab9cf725b1c3d30ed1b3fbc52b039997a8efa
zavet-code <yauheni.zavatski13@gmail.com>
2021-08-03 10:59:58 +0300

    with previus commit def https://drive.google.com/file/d/17_OB62X0ZUlXK-4fkqFZxdJABgBrxuw-/view?usp=sharing

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 import math
 
+
 def get_square_roots(num):
     defult_list = []
     defult_list.append(-math.sqrt(num))
     defult_list.append(math.sqrt(num))
     return print(defult_list)
 
-get_square_roots(25)
+def get_range(n):
+    defult_list1 = []
+    i = 0
+    while i < n:
+        defult_list1.append(i)
+        i += 1
+    if i > n and n == 0:
+        return print(defult_list1)
+    return print(defult_list1)
 
+get_range(5)
+get_range(0)
+get_range(-25)
