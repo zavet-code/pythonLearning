d5682e17c75406bd2dd448545a668404dece597d
zavet-code <yauheni.zavatski13@gmail.com>
2021-08-09 16:42:53 +0300

    url img random generator

--- screenshot_random.py
+++ screenshot_random.py
@@ -1,25 +1,38 @@
 import random
 import string
 import requests
+import re
 
 
 def generate_random_lightshot_url(length):
     letters = string.ascii_lowercase
-    letters_and_digits = string.ascii_letters + string.digits
+    letters_and_digits = string.ascii_lowercase + string.digits
     rand_string = ''.join(random.sample(random.choice([letters_and_digits,letters]), length))
     return "http://prnt.sc/" + rand_string
-
 
 
 def check_for_avai_img():
     page_url = generate_random_lightshot_url(6)
     html_text_url = requests.get(page_url, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36'}).text
     if '//st.prntscr.com/2021/04/08/1538/img/0_173a7b_211be8ff.png' in html_text_url:
-        return print('The screenshot was removed')
+        return 'The screenshot was removed'
     else:
-        return print(page_url)
+        return page_url
+
+def check_for_url_of_img(page_url):
+        if page_url == 'The screenshot was removed':
+            return print('The screenshot was removed')
+        html_text_url = requests.get(page_url, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36'}).text
+        if 'https://image.prntscr.com/image/' in html_text_url:
+            match = re.search('https://image.prntscr.com/image/.{22,32}.png', html_text_url)
+            print(match[0] if match else 'Not found')
+        else:
+            match = re.search('https://i.imgur.com/.{7}.png', html_text_url)
+            print(match[0] if match else 'Not found')
+
 
 i = 0
 while i < 30:
-    check_for_avai_img()
+    url = check_for_avai_img()
+    check_for_url_of_img(url)
     i += 1
