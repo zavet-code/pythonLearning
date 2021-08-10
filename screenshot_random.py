f66468c32216945848098a76f6603cb8a1ddecf7
zavet-code <yauheni.zavatski13@gmail.com>
2021-08-10 16:49:27 +0300

    fix bug removed screenshots

--- screenshot_random.py
+++ screenshot_random.py
@@ -21,7 +21,7 @@ def check_for_avai_img():
 
 def check_for_url_of_img(page_url):
         if 'The screenshot was removed' in page_url:
-            return 'https://github.com/'
+            return 'The screenshot was removed'
         html_text_url = requests.get(page_url, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36'}).text
         if 'https://image.prntscr.com/image/' in html_text_url:
             match = re.search('https://image.prntscr.com/image/.{22,32}.png', html_text_url)
@@ -32,8 +32,10 @@ def check_for_url_of_img(page_url):
 
 
 def download_img_from_url(url):
-    if ('Not found' or 'The screenshot was removed') in url:
+    if 'Not found' in url:
         print('Not found')
+    elif 'The screenshot' in url:
+        print('The screenshot was removed')
     else:
         filename = url.split('/')[-1]
         r = requests.get(url, allow_redirects=True)
