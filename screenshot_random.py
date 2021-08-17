995e178ab29b350e6fd51cb0d7854defda00af55
zavet-code <yauheni.zavatski13@gmail.com>
2021-08-17 11:12:13 +0300

    pip edit

--- screenshot_random.py
+++ screenshot_random.py
@@ -20,15 +20,15 @@ def check_for_avai_img():
         return page_url
 
 def check_for_url_of_img(page_url):
-        if 'The screenshot was removed' in page_url:
-            return 'The screenshot was removed'
-        html_text_url = requests.get(page_url, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36'}).text
-        if 'https://image.prntscr.com/image/' in html_text_url:
-            match = re.search('https://image.prntscr.com/image/.{22,32}.png', html_text_url)
-            return (match[0] if match else 'Not found')
-        else:
-            match = re.search('https://i.imgur.com/.{7}.png', html_text_url)
-            return (match[0] if match else 'Not found')
+    if 'The screenshot was removed' in page_url:
+        return 'The screenshot was removed'
+    html_text_url = requests.get(page_url, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36'}).text
+    if 'https://image.prntscr.com/image/' in html_text_url:
+        match = re.search('https://image.prntscr.com/image/.{22,32}.png', html_text_url)
+        return (match[0] if match else 'Not found')
+    else:
+        match = re.search('https://i.imgur.com/.{7}.png', html_text_url)
+        return (match[0] if match else 'Not found')
 
 
 def download_img_from_url(url):
