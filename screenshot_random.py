4ef8451457e5117b00282bda484add543fdc68ee
zavet-code <yauheni.zavatski13@gmail.com>
2021-08-30 17:00:07 +0300

    92percent download screenshots

--- screenshot_random.py
+++ screenshot_random.py
@@ -24,10 +24,10 @@ def check_for_url_of_img(page_url):
         return 'The screenshot was removed'
     html_text_url = requests.get(page_url, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36'}).text
     if 'https://image.prntscr.com/image/' in html_text_url:
-        match = re.search('https://image.prntscr.com/image/.{22,32}.png', html_text_url)
+        match = re.search('https://image.prntscr.com/image/\S+.\w+', html_text_url)
         return (match[0] if match else 'Not found')
     else:
-        match = re.search('https://i.imgur.com/.{7}.png', html_text_url)
+        match = re.search('https://i.imgur.com/\S+.\w+', html_text_url)
         return (match[0] if match else 'Not found')
 
 
@@ -42,7 +42,8 @@ def download_img_from_url(url):
         open(filename, 'wb').write(r.content)
 
 i = 0
-while i < 100:
+while i < 1000:
     url = check_for_avai_img()
+    print(url)
     download_img_from_url(check_for_url_of_img(url))
     i += 1
