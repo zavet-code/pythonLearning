37eee8c67d0b7947228cae550fdf8a81bfdb33af
zavet-code <yauheni.zavatski13@gmail.com>
2021-09-07 12:13:25 +0300

    fix format images and filename = datetime

--- screenshot_random.py
+++ screenshot_random.py
@@ -2,6 +2,7 @@ import random
 import string
 import requests
 import re
+import datetime
 
 
 def generate_random_lightshot_url(length):
@@ -37,8 +38,9 @@ def download_img_from_url(url):
     elif 'The screenshot' in url:
         print('The screenshot was removed')
     else:
-        filename = url.split('/')[-1]
-        r = requests.get(url, allow_redirects=True)
+        name = datetime.datetime.utcnow().strftime('%Y-%m-%d %H_%M_%S.%f')[:-3]
+        filename = "%s.png"% name
+        r = requests.get(url, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36'}, allow_redirects=True)
         open(filename, 'wb').write(r.content)
 
 i = 0
